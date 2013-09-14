5f0ef662035833cac4f1bef057ab448d500750ff
Sean J. Herman <seanherman@gmail.com>
2013-09-14 19:12:36 -0400

    ch2 reading done

---
+++ src/ch3/CP.java
@@ -0,0 +1,25 @@
+package ch3;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: seanh
+ * Date: 9/14/13 6:51 PM
+ */
+public class CP {
+    public static void main(String[] args) {
+        /*// 3.4
+        int y = 5;
+        int x = 0;
+        if (y > 0) {
+            x = 1;
+        }
+        System.out.println(x);*/
+
+        // 3.3
+        //boolean b = true;
+        //int i = (int)b;
+        //int i = 1;
+        //boolean b = (boolean)i;
+        //System.out.println(i);
+    }
+}
