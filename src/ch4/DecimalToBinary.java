2634a38bf03f09058c6143f41d2bbeae73610128
Sean J. Herman <seanherman@gmail.com>
2013-09-22 22:58:11 -0400

    bedward

---
+++ src/ch4/DecimalToBinary.java
@@ -0,0 +1,19 @@
+public class DecimalToBinary {
+	public static void main(String[] args) {
+	// Introduce the program
+	// Create a new Scanner
+	// Prompt the user for an input integer
+	// Find minimum/first power of 2 greater than the input value
+	// 6
+		// sumProducts = 1, power = 0
+		// do
+			// sumProducts *=2;
+			// power++;
+		//while (value > sumProducts);
+
+	// Cycle through modulo for each power of 2
+		// first, get the value of 2 ^ i
+		// if value % (power of 2) == 0, then print 1 else print 0
+	}
+
+}
