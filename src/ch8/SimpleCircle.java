44f5f0e27d8868fe2e891888275844e62c52e3ab
Sean Herman <sherman@sherman-PC.(none)>
2013-10-07 23:08:28 -0400

    more ch8

--- src/ch8/SimpleCircle.java
+++ src/ch8/SimpleCircle.java
@@ -1,27 +1,34 @@
 public class SimpleCircle {
     public static void main(String[] args) {
         // Create a new circle w/ radius 1
-        SimpleCircleTest circle1 = new SimpleCircleTest();
+        SimpleCircle circle1 = new SimpleCircle();
 
         System.out.println("The area of the circle of radius " + 
             circle1.radius + " is " + circle1.getArea());
 
+        SimpleCircle circle2 = new SimpleCircle(15);
+
+        System.out.println("The area of the circle of radius " + 
+            circle2.radius + " is " + circle2.getArea());
+
+        circle2.setRadius(50);
+
+        System.out.println("The area of the circle of radius " + 
+            circle2.radius + " is " + circle2.getArea());
+
 
     }
 
-}
-
-class SimpleCircleTest {
     /** The radius of this circle */
     double radius;
 
     /** Construct a circle object */
-    SimpleCircleTest() {
+    SimpleCircle() {
         radius = 1;
     }
 
     /** Construct a circle object */
-    SimpleCircleTest(double newRadius) {
+    SimpleCircle(double newRadius) {
         radius = newRadius;
     }
 
