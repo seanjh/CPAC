5ef751008a2d087cfb97ee42867f695ef1f46ae1
seanjh <seanherman@gmail.com>
2013-11-19 01:00:39 -0500

    hw8 wip

--- src/homework/hw6/Checkout.java
+++ src/homework/hw6/Checkout.java
@@ -1,3 +1,23 @@
+/**
+ * Author:  Sean Herman
+ * Date:    11/17/2013
+ * HW#:     7
+ * File:    Checkout.java
+ * Summary: This program implements a Dessert Shoppe stocked with various
+ *  Dessert Items. The available desserts are: Candy, Cookie, Ice Cream, and Sundae 
+ *  (an Ice Cream w/ 1 topping).
+ * Assumptions: I implemented an additional nested class in Sundae named SundaeTopping.
+ * I felt this was the right approach, since the topping on the Sundae *is* a
+ * DessertItem itself, and I wanted to ensure the DessertItem constructor logic was
+ * still available for this item, without duplicating that code. I made all its
+ * methods and data private (including its constructor), which means that no
+ * SundaeTopping objects may be created without creating a Sundae. The getCost method
+ * in SundaeTopping is public, however, because I found that (for good reason) it
+ * is not possible to override a parent public method with a method that has a
+ * different visibility modifier.
+ *
+ */
+
 public class Checkout {
     private DessertItem[] desserts;
     private int numberOfItems; // numberOfItems represents the last valid index
---
+++ src/homework/hw8/GameOfLife.java
@@ -0,0 +1,145 @@
+import java.util.Scanner;
+import java.io.File;
+
+public class GameOfLife {
+        public static final int M = 25; // rows
+        public static final int N = 75; // columns
+
+    public static void main(String args[]) {
+        // Create 2-dimensional array with edges on top, bottom, left, and right
+        char[][] world = new char[M + 2][N + 2];
+
+        Scanner consoleReader = new Scanner(System.in);
+
+        System.out.print ("Which file do you want to open? ");
+        
+        String filename = consoleReader.next();
+        File file = new File(filename);
+        Scanner fileReader = null;
+
+        // Open a data file
+        try
+        {
+            fileReader = new Scanner(file);        
+        } 
+        catch (Exception e) 
+        {
+            System.out.print("file " + file + " does not exist.\n");
+            System.exit(0);
+        }
+
+        // Fill the edges of the array with empty values ('.')
+        emptyEdges(world);
+        
+
+        // Fill the body of the array with the input data
+        for (int i = 1; i <= M; i++)
+        {
+            String line = fileReader.nextLine();
+            System.out.println(line);
+            char[] lineCharacters = line.toCharArray();
+            for (int j = 1; j <= N; j++) {
+                world[i][j] = lineCharacters[j - 1];
+            }
+        }
+
+        // event loop
+        System.out.print("Enter Q to Quit, or Enter another character to see the next Generation. ");
+        String input = consoleReader.next();
+
+        char[][] oldWorld;
+
+        int generation = 0;
+
+        while(!input.toLowerCase().equals("q")) {
+            generation++;
+
+            oldWorld = world;
+            world = new char[oldWorld.length][oldWorld[0].length];
+
+            int neighbors;
+            for (int i = 1; i <= GameOfLife.M; i++) {
+                for (int j = 1; j <= GameOfLife.N; j++) {
+                    neighbors = numberOfNeighbors(oldWorld, i, j);
+
+                    switch(oldWorld[i][j]) {
+                        case ('X'):
+                            if (neighbors == 2 || neighbors == 3)
+                                world[i][j] = 'X'; // organism survives
+                            else
+                                world[i][j] = '.'; // organism dies
+                            break;
+                        case ('.'):
+                            if (neighbors == 3)
+                                world[i][j] = 'X'; // new organism
+                            break;
+                    }
+
+                }
+            }
+
+            // Display the generation
+            System.out.printf("Generation #%d\n", generation);
+            printWorld(world);
+
+            //
+            System.out.print("Enter Q to Quit, or Enter another character to see the next Generation. ");
+            input = consoleReader.next();
+        }
+        
+    } // end main
+
+    public static void printWorld(char[][] world) {
+        for (int i = 1; i <= GameOfLife.M; i++) {
+            for (int j = 1; j <= GameOfLife.N; j++) {
+                System.out.print(world[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void emptyEdges(char[][] world) {
+        int lastColumn = world[0].length - 1;
+        for (int i = 0; i < world.length; i++) {
+            if (i == 0 || i == lastColumn) { // top and bottom row
+                for (int j = 0; j < world[0].length; j++) {
+                    world[i][j] = '.';
+                }
+
+            } else {
+                world[i][0] = '.'; // first column of each row
+                world[i][lastColumn] = '.'; // last column of each row
+            }
+        }
+    }
+
+    public static int numberOfNeighbors(char[][] world, int x, int y) {
+        int neighbors;
+        if (world[x][y] == 'X')
+            neighbors = -1; // exclude self
+        else
+            neighbors = 0;
+
+        // Check in array from world[x-1][y-1] through world[x+1][y+1]
+        for (int i = -1; i <= 1; i++ ) {
+            for (int j = -1; j <= 1; j++) {
+                if (world[i][j] == 'X')
+                    neighbors++;
+            }
+        }
+
+        return neighbors;
+    }
+
+    public static boolean isEmpty(char[][] world) {
+        for (int i = 1; i <= GameOfLife.M; i++) {
+            for (int j = 1; j <= GameOfLife.N; j++) {
+                if (world[i][j] == 'X')
+                    return false; // Found life, array is not empty
+            }
+        }
+
+        return true; // Did not find life
+    }
+
+}
