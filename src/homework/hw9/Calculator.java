765b5bd72374dfc1d3da3b45b91c8869076b1a98
seanjh <seanherman@gmail.com>
2013-12-03 21:14:56 -0500

    hw9 refinements

--- src/homework/hw9/Calculator.java
+++ src/homework/hw9/Calculator.java
@@ -1,16 +1,36 @@
 /*
-* File:     Calculator.java
-* Author:   Sean Herman
-* Date:     12/2/2013
+* File:         Calculator.java
+* Author:       Sean Herman
+* Date:         12/03/2013
+* Assignment:   #9
+* Summary:      Converts an input infix expression String into its postfix 
+    equivalent, and evaluates the result.
+
+* Assumptions:  Updated PostFixEvaluator to add the ^, and to return a double. 
+*   The original class returned an int, but this obviously produces bad results 
+*   for expressions with division. The assignment does show the result as 
+*   a decimal value (3.0), so I assume this was a requirement.
+*
+*   I also added a few helper classes to aid in tokenizing the input infix 
+*   expression (InfixIterator), and process/validate operators (Operator) 
+*   Finally, I also added an InfixException class in the spirit of the 
+*   PostFixException class provided by the Dale book. Since this class is used 
+*   by Calculator and Converter, I've included that code in a separate file. 
+*   Ideally, all these helper classes would be in separate .java source
+*   files, but I felt it was probably easier for grading to lump them with the
+*   2 required files, Converter.java and Calculator.java.
+*
+*   Execution requires the following 8 files from the Dale book:
+*       ArrayStack.java, StackInterface.java, BoundedStackInterface.java, 
+*       StackUnderflowException.java, StackOverflowException.java, 
+*       ArrayUnsortedList.java, ListInterface.java, and PostFixException.java
 * 
 */
 
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
-public class Calculator {
-    public static final boolean DEBUG = false;
-    
+public class Calculator {    
     public static void main(String[] args) {
 
         System.out.println("\nWelcome to Infix Calculator.\n");
@@ -68,6 +88,83 @@ public class Calculator {
 
         public boolean isQuit() {
             return input.toLowerCase().equals("q");
+        }
+    }
+
+    // PostFixEvaluator evaluates a postfix expression String.
+    private static class PostFixEvaluator {
+        public static double evaluate(String expression) throws PostFixException {
+            BoundedStackInterface<Double> stack = new ArrayStack<Double>(50);
+
+            int value;
+            String operator;
+
+            double operand1;
+            double operand2;
+
+            double result = 0;
+
+            Scanner tokenizer = new Scanner(expression);
+
+            while (tokenizer.hasNext()) {
+                if (tokenizer.hasNextInt()) {
+                    value = tokenizer.nextInt();
+                    // Process the operand
+                    
+                    if (stack.isFull())
+                        throw new PostFixException("Too many operands. Stack Overflow");
+                    
+                    stack.push((double)value);
+                } else {
+                    // Process the operator
+                    operator = tokenizer.next();
+
+                    // Obtain second operand from stack.
+                    if (stack.isEmpty())
+                        throw new PostFixException("Not enough operands - stack underflow");
+                    
+                    operand2 = stack.top();
+                    stack.pop();
+                    
+                    // Obtain first operand from stack.
+                    if (stack.isEmpty())
+                        throw new PostFixException("Not enough operands - stack underflow");
+
+                    operand1 = stack.top();
+                    stack.pop();
+
+                    // Perform operation
+                    if (operator.equals("/") | operator.equals("÷"))
+                        result = operand1 / operand2;
+                    else if (operator.equals("*") | operator.equals("x"))
+                        result = operand1 * operand2;
+                    else if (operator.equals("+"))
+                        result = operand1 + operand2;
+                    else if (operator.equals("-"))
+                        result = operand1 - operand2;
+                    else if (operator.equals("^"))
+                        result = (int)(Math.pow(operand1, operand2));
+                    else
+                        throw new PostFixException("Illegal symbol: " + operator);
+
+                    // Push result to the stack
+                    stack.push(result);
+                }
+            }
+
+            // Obtain the final result from stack
+            if (stack.isEmpty())
+                throw new PostFixException("Not enough operands - stack underflow");
+            
+            result = stack.top();
+            stack.pop();
+
+            // Stack should now be empty.
+            if (!stack.isEmpty())
+                throw new PostFixException("Too many operands - operands left over");
+
+            // Return the final result
+            return result;
         }
     }
 }
--- src/homework/hw9/Converter.java
+++ src/homework/hw9/Converter.java
@@ -1,15 +1,20 @@
-//
-// Converter.java
-// 
-// Converts infix-style expressions into postfox-style expressions.
-// 
-//  Input:  String representing a valid infix-style expression
-//  Output: String representing the corresponding postfix-style expression
+/*
+* File:         Converter.java
+* Author:       Sean Herman
+* Date:         12/03/2013
+* Assignment:   #9
+* Summary:      Converts infix-style expressions into postfox-style expressions.
+*       Input is a String representing a valid infix-style expression.
+        Output is a String representing the corresponding postfix-style 
+        expression.
+*/
+
+import java.util.NoSuchElementException;
+import java.util.Iterator;
 
 public class Converter {
-    
-    private String infix;
-    private String postfix;
+    private String infix;       // input infix expression
+    private String postfix;     // derived postfix equivalent expression
     BoundedStackInterface<String> stack;
 
     public Converter(String infix) {
@@ -26,10 +31,7 @@ public class Converter {
         InfixIterator tokenizer = new InfixIterator(infix);
         String token;
 
-        if (Calculator.DEBUG)
-            System.out.println(tokenizer);
-
-        while(tokenizer.hasNext()) {
+        while(tokenizer.hasNext()) { // Keep looping while input token Strings remain
             token = tokenizer.next();
 
             if (Character.isDigit(token.charAt(0))) { // Operands always start with a digit
@@ -41,41 +43,23 @@ public class Converter {
             } else {
                 processOperator(new Operator(token));
             }
-
-            if (Calculator.DEBUG){
-                printStatus(token);
-                System.out.println();
-            }
         }
 
-        while (!stack.isEmpty()) {
-            if (Calculator.DEBUG){
-                System.out.println("Clearing stack.");
-                printStatus(new String(""));
-            }
-            postfix += stack.top() + " "; // append operators left in stack
+        while (!stack.isEmpty()) { // clear out any remaining operators
+            postfix += stack.top() + " "; // append these leftovers postfix
             stack.pop();
         }
-
-        if (Calculator.DEBUG)
-                printStatus(new String(""));
 
         return postfix;
     }
 
-    private void printStatus(String token) {
-        System.out.print("Token: " + token + ", ");
-        System.out.print("Stack: " + stack + ", ");
-        System.out.println("Postfix: " + postfix);
-    }
-
     private void processCloseParens() {
         while (!stack.isEmpty()) {
-            if (stack.top().equals("(")) { 
-                stack.pop();
-                break; // Pop out the matching parens and break
-            } else {
-                postfix += (stack.top() + " ");
+            if (stack.top().equals("(")) {
+                stack.pop(); // pop out the matching open parens
+                break; // and stop looping through the stack
+            } else { 
+                postfix += (stack.top() + " "); // append top operator to postfix
                 stack.pop();
             }
         }
@@ -87,7 +71,7 @@ public class Converter {
             return; // always push operators on empty stack or for parens at top
         } else { // there's an operator at the top of the stack
             Operator stackOp = new Operator(stack.top());
-            if (stackOp.compareTo(tokenOp) >= 0) {
+            if (stackOp.compareTo(tokenOp) >= 0) { // i.e., if (this op >= stack op)
                 postfix += stack.top() + " "; // append stack top's lower/equal rank operator
                 stack.pop();
                 processOperator(tokenOp);  // then do a recursive call
@@ -100,5 +84,144 @@ public class Converter {
     @Override
     public String toString() {
         return postfix;
+    }
+}
+
+class Operator implements Comparable<Operator> {
+    private String operator;
+    private int rank;
+
+    private static final String[] OPERATORS = {
+        "+", "-", "*", "x", "/", "÷", "^"
+    };
+    
+    private static final String[][] OPERATOR_RANKS = {
+        // 2-dimensional array (i.e., X,Y). The 1st dimension (X) index value
+        // corresponds to the operator's rank.
+        {"-", "+"}, // rank 0 (lowest)
+        {"*", "/", "x", "÷"}, // rank 1
+        {"^"} // rank 2 (highest)
+    };
+
+    public Operator(String operator) throws IllegalArgumentException {
+        if (isOperator(operator))
+            this.operator = operator;
+        else 
+            throw new IllegalArgumentException("Invalid operator input");
+
+        // Determine the rank of the operator
+        for (int i = 0; i < OPERATOR_RANKS.length; i++)
+            for (int j = 0; j < OPERATOR_RANKS[i].length; j++)
+                if (operator.equals(OPERATOR_RANKS[i][j])) {
+                    rank = i;
+                    return;
+                }
+    }
+
+    public static boolean isOperator(String input) {
+        for (int i = 0; i < OPERATORS.length; i++) {
+            if (input.equals(OPERATORS[i]))
+                return true;
+        }
+        return false;
+    }
+
+    public static boolean isOperator(char input) {
+        Character c = (Character) input;
+        return isOperator(c.toString());
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    @Override
+    public int compareTo(Operator op) {
+        // Compares this operator's rank to the parameter's rank
+        if (rank < op.rank)
+            return -1; // this operator's rank is less than (lower precedence)
+        else if (op.rank == rank)
+            return 0; // ranks are equal
+        else
+            return 1; // this operator's rank is greater than (higer precedence)
+    }
+}
+
+class InfixIterator implements Iterator<String> {
+    private String infix;
+    private ListInterface<String> tokens;
+    private String intBuffer;
+    private int currentElement;
+
+    public InfixIterator(String infix) throws InfixException {
+        this.infix = infix;
+        tokens = new ArrayUnsortedList(infix.length());
+        currentElement = 0;
+
+        // Translate the string into a list of Character objects
+        Character[] charArray = new Character[infix.length()];
+        for (int i = 0; i < charArray.length; i++) {
+            charArray[i] = new Character(infix.charAt(i));
+        }
+
+        intBuffer = ""; // accumulates adjacent digits in the String
+        for (Character ch : charArray) {
+            if (Character.isDigit(ch)) { 
+                intBuffer += ch.toString(); // append digits to the int buffer
+            } else if (Operator.isOperator(ch)) {
+                addBuffer(); // add int buffer (if applicable)
+                tokens.add(ch.toString()); // add operators to the list
+            } else if (ch.equals('(') || ch.equals(')')) {
+                addBuffer(); // add int buffer (if applicable)
+                tokens.add(ch.toString()); // add parens to the list
+            } else if (Character.isWhitespace(ch)) {
+                addBuffer(); // add int buffer (if applicable)
+                // Do not add spaces
+            } else {
+                throw new InfixException("Invalid character in infix " +
+                "expression: " + ch);
+            }
+        }
+
+        addBuffer(); // clear out any remaining buffer
+    }
+
+    public String getInfix() {
+        return infix;
+    }
+
+    private void addBuffer() {
+        if (!intBuffer.equals("")) {
+            tokens.add(intBuffer); // add the accumulated int buffer to the list
+            intBuffer = ""; // reset the String buffer
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return currentElement < tokens.size();
+    }
+
+    @Override
+    public String next() throws NoSuchElementException {
+        currentElement++;
+        return tokens.getNext();
+    }
+
+    @Override
+    public void remove() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String toString() {
+        String out = "";
+        out += ("Inflix expression: " + infix + "\n");
+        out += tokens.toString();
+        return out;
     }
 }
